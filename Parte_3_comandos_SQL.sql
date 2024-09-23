88d14c4c3943086e00fb6179aecdc1b92225d97e
Adriana da Mata <58087621+adrianadamata@users.noreply.github.com>
2024-09-22 22:26:43 -0300

    Estrutura para completar com informações do Banco de Dados

---
+++ Parte_3_comandos_SQL.sql
@@ -0,0 +1,17 @@
+-- Parte 3 Tarefas práticas:
+-- Agora, execute os comandos SQL para realizar as seguintes operações:
+-- Renomear a coluna nome_professor para nome_prof na tabela tbProfessor:
+ALTER TABLE ___ RENAME COLUMN ___ TO ___;
+
+-- Remover a coluna telefone_aluno da tabela tbAluno:
+ALTER TABLE ___ DROP COLUMN ___;
+
+-- Inserir dois novos registros na tabela tbAluno:
+INSERT INTO ___ (___, ___)
+VALUES 
+(___, ___),
+(___, ___);
+
+-- Consulta para mostrar todos os dados dos alunos ordenados por nome_aluno em ordem crescente:
+SELECT __ FROM ___ ORDER BY ___ ASC;
+
